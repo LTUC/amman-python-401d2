ede1ccbd34a22d778bea0eb4b1adfeb39fd8d877
AyaAbuRashed <ayanaser402@gmail.com>
2020-11-29 21:46:11 +0200

    challenge1

--- data-structures-and-algorithms-python/data_structures_and_algorithms/challenges/array_reverse/array_reverse.py
+++ data-structures-and-algorithms-python/data_structures_and_algorithms/challenges/array_reverse/array_reverse.py
@@ -1,12 +1,21 @@
-def reverse_array(arr):
-    """Reverses a list
+def reverse_array():
 
-    Args:
-        arr (list): python list
+ # put your function implementation here
+ list = []
+ n = int(input("Enter number of elements : "))
+ for i in range(0, n):
+    element = int(input())
+    list.append(element) # adding the element
 
-    Returns:
-        [list]: list in reversed form
-    """
-    # put your function implementation here
-    return arr
+ list = list[::-1]
+ print(list)
+
+reverse_array()
+
+def reverse_array_(arr):
+
+# put your function implementation here
+
+   arr.reverse()
+   return arr
 
