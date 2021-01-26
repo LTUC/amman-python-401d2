50404aa1d715cee26405c4a57a3f1e691ef21d58
obiorbitalstar <obiorbitalstar@gmail.com>
2021-01-26 14:56:24 +0200

    fix

---
+++ class-43/demo/full-stack-auth/frontend/frontendapi/src/App.js
@@ -0,0 +1,26 @@
+import React,{useState} from 'react';
+import './App.css';
+import Login from './components/login';
+import Books from './components/books'
+
+
+function App() {
+
+  const [token,setToken] = useState('');
+
+  const userLogin = (tok) =>{
+    setToken(tok)
+    console.log(tok);
+    
+  }
+
+
+  return (
+    <div className="App">
+     <Login userLogin={userLogin}/>
+     <Books token={token}/>
+    </div>
+  );
+}
+
+export default App;
